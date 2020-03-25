3b87163dd6f0ea9de78c9b80c56654281940778a
Jake Shadle <jake.shadle@embark-studios.com>
2020-03-25 10:59:30 +0100

    Add panic hook fail repro

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
 extern crate wasmer_runtime;
 
-use std::sync::{Arc, Mutex};
+use std::{
+    fmt,
+    sync::{Arc, Mutex},
+};
 use wasmer_runtime::{error, func, imports, instantiate, Array, Ctx, WasmPtr};
 
 // Make sure that the compiled wasm-sample-app is accessible at this path.
 static WASM: &'static [u8] =
     include_bytes!("../wasm-sample-app/target/wasm32-unknown-unknown/release/wasm_sample_app.wasm");
+
+#[derive(Debug)]
+pub struct PanicLocation {
+    pub file: std::path::PathBuf,
+    pub line: u32,
+    pub column: u32,
+}
+
+/// Represents a panic that was caught inside the wasm module
+#[derive(Debug)]
+pub struct PanicInfo {
+    /// The error message for the panic
+    pub message: String,
+    /// The location of the panic, if available
+    pub location: Option<PanicLocation>,
+}
+
+impl fmt::Display for PanicInfo {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(&self.message)?;
+
+        if let Some(loc) = &self.location {
+            write!(f, ", {}:{}:{}", loc.file.display(), loc.line, loc.column)?;
+        }
+
+        Ok(())
+    }
+}
 
 fn main() -> error::Result<()> {
     // create shared data that we'll use in 2 host functions
@@ -32,6 +63,33 @@ fn main() -> error::Result<()> {
         let mut guard = data.lock().unwrap();
         *guard += 1;
     };
+
+    let panic_info = Arc::new(Mutex::new(None));
+    let pi = panic_info.clone();
+    let register_panic = move |ctx: &mut wasmer_runtime::Ctx,
+                               err_msg_ptr: WasmPtr<u8, Array>,
+                               err_msg_len: u32,
+                               loc_file_ptr: WasmPtr<u8, Array>,
+                               loc_file_len: u32,
+                               loc_line: u32,
+                               loc_column: u32| {
+        let memory = ctx.memory(0);
+        let err_msg = err_msg_ptr.get_utf8_string(memory, err_msg_len).unwrap();
+
+        let file = loc_file_ptr.get_utf8_string(memory, loc_file_len).unwrap();
+
+        let panic_info = PanicInfo {
+            message: err_msg.to_owned(),
+            location: Some(PanicLocation {
+                file: file.into(),
+                line: loc_line,
+                column: loc_column,
+            }),
+        };
+
+        (*pi.lock().unwrap()) = Some(panic_info);
+    };
+
     // Let's define the import object used to import our function
     // into our webassembly sample application.
     //
@@ -52,6 +110,7 @@ fn main() -> error::Result<()> {
             // we can use closures here too
             "print_str2" => func!(print_str2),
             "increment_shared" => func!(increment_shared),
+            "register_panic" => func!(register_panic),
         },
     };
 
@@ -60,6 +119,23 @@ fn main() -> error::Result<()> {
 
     // Call our exported function!
     instance.call("hello_wasm", &[])?;
+
+    for i in 0..4 {
+        // Reset panic information before every call to ensure a previous
+        // panic doesn't spill
+        (*panic_info.lock().unwrap()) = None;
+        match instance.call("fails", &[]) {
+            Ok(_) => panic!("calling 'fails' should have returned an error"),
+            Err(_) => match *panic_info.lock().unwrap() {
+                Some(ref pi) => {
+                    println!("call #{} to 'fails' correctly captured panic '{}'", i, pi);
+                }
+                None => {
+                    println!("call #{} to 'fails' failed to capture panic information", i);
+                }
+            },
+        }
+    }
 
     Ok(())
 }
--- wasm-sample-app/src/lib.rs
+++ wasm-sample-app/src/lib.rs
@@ -7,6 +7,14 @@ extern "C" {
     fn print_str(ptr: *const u8, len: usize);
     fn print_str2(ptr: *const u8, len: usize);
     fn increment_shared();
+    fn register_panic(
+        msg_ptr: *const u8,
+        msg_len: u32,
+        file_ptr: *const u8,
+        file_len: u32,
+        line: u32,
+        column: u32,
+    );
 }
 
 // Define a string that is accessible within the wasm
@@ -36,4 +44,56 @@ pub extern "C" fn hello_string_from_rust(ptr: i32, len: i32) {
     unsafe {
         print_str(out_str.as_ptr(), out_str.len());
     }
+}
+
+#[no_mangle]
+pub extern "C" fn fails() {
+    register_panic_hook();
+
+    panic!("oh no");
+}
+
+fn hook(info: &std::panic::PanicInfo<'_>) {
+    let error_msg = info
+        .payload()
+        .downcast_ref::<String>()
+        .map(String::as_str)
+        .or_else(|| info.payload().downcast_ref::<&'static str>().copied())
+        .unwrap_or("");
+    let location = info.location();
+
+    unsafe {
+        let _ = match location {
+            Some(loc) => {
+                let file = loc.file();
+                let line = loc.line();
+                let column = loc.column();
+
+                register_panic(
+                    error_msg.as_ptr(),
+                    error_msg.len() as u32,
+                    file.as_ptr(),
+                    file.len() as u32,
+                    line,
+                    column,
+                )
+            }
+            None => register_panic(
+                error_msg.as_ptr(),
+                error_msg.len() as u32,
+                std::ptr::null(),
+                0,
+                0,
+                0,
+            ),
+        };
+    }
+}
+
+fn register_panic_hook() {
+    use std::sync::Once;
+    static SET_HOOK: Once = Once::new();
+    SET_HOOK.call_once(|| {
+        std::panic::set_hook(Box::new(hook));
+    });
 }
